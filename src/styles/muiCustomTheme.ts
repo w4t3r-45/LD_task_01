1312cf2c71e632167ec13e7dd17a869cf0196ef7
w4t3r-45 <webhost48@gmail.com>
2022-06-10 01:24:35 +0200

    add material UI custom theme to use custom fonts and colors

---
+++ src/styles/muiCustomTheme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+// used to change font family
+// we can change as much properties as we need
+// see here : https://mui.com/material-ui/customization/default-theme/#main-content
+
+export const OwnTheme = createTheme({
+  palette: {
+    success: {
+      main: "#00C48C",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    fontFamily: '"Noto Sans HK" , "Helevetica" , "Arial" , sans-serif ',
+  },
+});
