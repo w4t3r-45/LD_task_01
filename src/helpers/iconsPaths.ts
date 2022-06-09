f47d98dd3351a8902426531ff8b25c82c2a14412
w4t3r-45 <webhost48@gmail.com>
2022-06-10 01:21:36 +0200

    add svg paths export file

---
+++ src/helpers/iconsPaths.ts
@@ -0,0 +1,6 @@
+const ICONS_PATH = "/assets/images/drawer/icons";
+
+export const ARROW_UP_PATH = `${ICONS_PATH}/arrow_up.svg`;
+export const ARROW_DOWN_PATH = `${ICONS_PATH}/arrow_down.svg`;
+
+export const LOGO_PATH = `/assets/images/logo.svg`;
