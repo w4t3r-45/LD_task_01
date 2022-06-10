f19c7028a144fa46901914951d800b2ccf6279f3
w4t3r-45 <webhost48@gmail.com>
2022-06-10 16:36:58 +0200

    add : custom select box cpnnt

---
+++ src/components/CustomSelect.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Box, MenuItem, FormControl, Typography } from "@mui/material";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { SelectMenuProps } from "../styles/muiCustomStyles";
+import { SelectBoxType } from "../@types/selectBoxType";
+
+const handleChange = (
+  event: SelectChangeEvent,
+  setSvalue: React.Dispatch<React.SetStateAction<string>>
+) => {
+  console.log("hello");
+  setSvalue(event.target.value);
+};
+
+export const CustomSelect: React.FC<SelectBoxType> = ({
+  data,
+  title,
+}: SelectBoxType) => {
+  const [value, setSvalue] = useState(data[0]);
+
+  return (
+    <Box sx={{ padding: "0 20px" }}>
+      <FormControl
+        fullWidth
+        sx={{
+          width: "115px",
+          "& .MuiOutlinedInput-root": {
+            borderRadius: "5px",
+            width: "215px",
+            height: "50px",
+            fontSize: "1rem",
+            fontWeight: 500,
+          },
+        }}
+      >
+        <Typography variant="body1" fontSize="15px" mb="15px">
+          {title}
+        </Typography>
+        <Select
+          onChange={(event: SelectChangeEvent) =>
+            handleChange(event, setSvalue)
+          }
+          value={value}
+          MenuProps={SelectMenuProps}
+          // inputProps={{ "aria-label": "Without label" }}
+        >
+          {data.map((item) => (
+            <MenuItem value={item} sx={{ minWidth: "115px" }}>
+              {item}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
