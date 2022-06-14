6c0d666380bc788cd2566ea42f79d204782ef623
w4t3r-45 <webhost48@gmail.com>
2022-06-14 23:07:38 +0200

    added mock data for testing

---
+++ src/mock/SLIDER_DATA_MOCK.ts
@@ -0,0 +1,40 @@
+import {
+  EyeIcon,
+  ArrowDownIcon,
+  ArrowRightIcon,
+  CreditCardIcon,
+} from "../components/icons";
+
+export const SLIDER_DATA_MOCK = [
+  {
+    TopChild: EyeIcon,
+    title: "random Image  1",
+    bgcolor: "green",
+  },
+  {
+    TopChild: EyeIcon,
+
+    title: "random Image  2",
+    bgcolor: "red",
+  },
+  {
+    TopChild: ArrowDownIcon,
+    title: "random Image  3",
+    bgcolor: "pink",
+  },
+  {
+    TopChild: EyeIcon,
+    title: "random Image  4",
+    bgcolor: "purple",
+  },
+  {
+    TopChild: CreditCardIcon,
+    title: "random Image  5",
+    bgcolor: "yellow",
+  },
+  {
+    TopChild: ArrowRightIcon,
+    title: "random Image  6",
+    bgcolor: "brown",
+  },
+];
