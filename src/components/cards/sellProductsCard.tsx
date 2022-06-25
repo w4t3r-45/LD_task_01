5e690f2d509cb4b472070d004c0a1e054e698fed
w4t3r-45 <webhost48@gmail.com>
2022-06-25 14:55:39 +0200

    fix sellProduct card height

--- src/components/cards/sellProductsCard.tsx
+++ src/components/cards/sellProductsCard.tsx
@@ -14,7 +14,7 @@ export const SellProductsCard = () => {
         boxShadow:
           "0px 4px 4px rgba(50, 50, 71, 0.08), 0px 4px 8px rgba(50, 50, 71, 0.06)",
         background: "#F3A35C",
-        // height: "349px",
+        height: "349px",
       }}
       CardBody={
         <div style={{ maxWidth: "450px", margin: "auto", marginTop: "-20px" }}>
